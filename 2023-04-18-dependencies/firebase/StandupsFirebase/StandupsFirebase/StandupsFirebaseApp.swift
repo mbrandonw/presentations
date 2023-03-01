11acff9d8eb4190a7af29367e4638fa90ac337f9
Brandon Williams <mbrandonw@hey.com>
2023-03-01 13:13:18 -0800

    wip

---
+++ 2023-04-18-dependencies/firebase/StandupsFirebase/StandupsFirebase/StandupsFirebaseApp.swift
@@ -0,0 +1,25 @@
+import FirebaseCore
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct YourApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
