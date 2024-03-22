829c1774ddbb652d3a4788004c4897ba5eb2499c
Brandon Williams <mbrandonw@hey.com>
2024-03-22 09:53:59 +0900

    wip

--- 2024-03-22-macros/swift-case-paths/Tests/CasePathsMacrosTests/CasePathableMacroTests_TrySwift.swift
+++ 2024-03-22-macros/swift-case-paths/Tests/CasePathsMacrosTests/CasePathableMacroTests_TrySwift.swift
@@ -5,7 +5,7 @@ import SwiftSyntaxMacrosTestSupport
 import XCTest
 
 final class CasePathableMacroTests_TrySwift: XCTestCase {
-  func testBasics() {
-    
+  func testBasics_AssertMacroExpansion() {
+
   }
 }
